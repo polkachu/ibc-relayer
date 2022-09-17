b1c78bf05b8cdb2be7c998a5843cfdf50a552e91
Polkachu <hello@polkachu.com>
2022-09-16 17:24:53 -0700

    update

--- all.sh
+++
@@ -1,7 +0,0 @@
-ansible-playbook main_hermes_config.yml -e "target=juno"
-ansible-playbook main_hermes_config.yml -e "target=osmosis"
-ansible-playbook main_hermes_config.yml -e "target=gravity"
-ansible-playbook main_hermes_config.yml -e "target=kujira"
-ansible-playbook main_hermes_config.yml -e "target=injective"
-ansible-playbook main_hermes_config.yml -e "target=stride"
-ansible-playbook main_hermes_config.yml -e "target=evmos"
